aec4d18645af9cc5f1a122160679b8e8735fc96d
Mandar Bhalerao <mmbhaler@uci.edu>
2024-01-21 19:24:31 -0800

    changed menu page layout

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 import CartModal from "../components/cartModal";
 
 const Menu = ()=>{
@@ -41,24 +40,30 @@ const Menu = ()=>{
     }
 
     return (
-        <div className="d-flex flex-wrap justify-content-center">
-            {Object.keys(restaurantDetails).length > 0 && restaurantDetails.menu.map((item,index) => (
-                <div key={index}>
-                    <Card style={{ width: '18rem', margin: '15px' }}>
-                    <Card.Body>
-                        <Card.Title>{item.item}</Card.Title>
-                        <Card.Text>
-                        {item.description}
-                        ${item.price}
-                        </Card.Text>
-                        <Button variant="primary" onClick={()=>addToCart(index)}>Add to Cart</Button>
-                    </Card.Body>
-                    </Card>
+      <Row className="justify-content-center m-2">
+      {Object.keys(restaurantDetails).length > 0 &&
+        restaurantDetails.menu.map((item, index) => (
+          <Col key={index} lg={4} md={6} sm={12} className="mb-4">
+            <Card className="mb-4 d-flex flex-column h-100">
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{item.item}</Card.Title>
+                <Card.Text className="flex-grow-1">
+                  {item.description}
+                  <br />
+                  <strong>${item.price}</strong>
+                </Card.Text>
+                <div className="text-center">
+                  <Button variant="primary" onClick={() => addToCart(index)}>
+                    Add to Cart
+                  </Button>
                 </div>
-            ))}
-            <CartModal show={modalShow} setShow={setModalShow} itemIndex={itemIndex} restaurantDetails={restaurantDetails}/>
-            
-        </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+    
+      <CartModal show={modalShow} setShow={setModalShow} itemIndex={itemIndex} restaurantDetails={restaurantDetails} />
+    </Row>
             
     )
 }
